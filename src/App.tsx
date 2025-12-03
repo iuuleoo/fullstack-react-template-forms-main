c04389592edbea0e04b96613baf47f3a410b86c0
Leo <leooproencaaa@gmail.com>
2025-12-03 16:54:25 -0300

    Data

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Controller, useForm } from "react-hook-form"
 
 type FormData = {
   name: string
+  date: string
 }
 
 
 export default function App() {
   const { control, handleSubmit } = useForm<FormData>({ defaultValues: {
     name: "",
+    date: "",
   },
 })
 
@@ -31,8 +33,15 @@ export default function App() {
           
         <span className="error">Nome é obrigatório</span>
 
+        
 
-        <input type="date" placeholder="Nome do evento" lang="pt-BR" />
+        <Controller 
+        control={ control }
+        name="date"
+        render={({ field }) => (
+          <input type="date" placeholder="Nome do evento" lang="pt-BR" {...field} />
+        )}
+        />
 
         <select defaultValue="">
           <option value="" disabled>
